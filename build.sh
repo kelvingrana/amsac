f85c1c603250f26486dca00666b7fcba3be43043
kelvingrana <kelvin.grana@gmail.com>
2025-01-23 15:06:31 -0600

    Add .gitattributes to enforce LF line endings

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 
-set -0 errexit
+set -o errexit
 
-pip install -r requirements.txt
+# pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
