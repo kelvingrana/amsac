93b2620ea7d2b9175e46c97f8fc2202bce59ac72
kelvingrana <kelvin.grana@gmail.com>
2025-01-23 15:41:13 -0600

    Corregir error tipográfico en settings.py

--- project_amsac/settings.py
+++ project_amsac/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 if not DEBUG:
-    STATIC_ROOT = os.phat.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     
 
